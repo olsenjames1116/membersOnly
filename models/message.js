a53d9cc5bf44df23788e5e2ab26a7c23eee6a3e1
James Olsen <olsenjames1116@gmail.com>
2023-12-11 21:53:13 -0500

    Display a formatted date for messages

--- models/message.js
+++ models/message.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -6,6 +7,11 @@ const MessageSchema = new Schema({
 	text: { type: String, required: true },
 	timestamp: { type: Date, required: true },
 	user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+});
+
+// Virtual for the message creation date formatted.
+MessageSchema.virtual('timestampFormatted').get(function () {
+	return DateTime.fromJSDate(this.timestamp).toLocaleString(DateTime.DATE_MED);
 });
 
 // Export model.
